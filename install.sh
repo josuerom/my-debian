921df9c8d14c4704c0a224a3179cd6ea2a7dea61
Josué Romero <79048209+josuerom@users.noreply.github.com>
2024-03-28 16:23:49 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo -e "\nHola Hackermen! script creado por @josuerom!"
-echo -e "Las instalaciones solo funcionan en Debian Linux\n"
+echo -e "Las instalaciones solo funcionan en Debian Linux, por los dos útlimos paquetes de openjdk\n"
 
 sudo apt update -y && sudo apt upgrade -y
-sudo apt install curl wget xclip vim ranger htop cmatrix g++ default-jdk default-jre snapd -y
+sudo apt install curl wget xclip vim ranger htop cmatrix g++ snapd default-jdk default-jre -y
 sudo apt purge firefox -y
 sudo cp -r .fonts/* /usr/share/fonts/
 sudo snap install chromium
@@ -11,10 +11,10 @@ sudo snap install spotify
 sudo snap install notion-snap-reborn
 sudo snap install teams-for-linux
 sudo snap install code --classic
-sudo apt install .deb-install/ulauncher_5.15.6_all.deb -y
-sudo apt install .deb-install/cpeditor-6.11.2-linux-amd64.deb -y
+sudo apt install .deb/ulauncher_5.15.6_all.deb -y
+sudo apt install .deb/cpeditor-6.11.2-linux-amd64.deb -y
 
-sudo mv cf /bin/
+sudo mv cf /usr/bin/
 sudo mv .* ~/
 mkdir -p ~/workspace ~/workspace/bin
 
